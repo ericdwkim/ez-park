5d688c7714a18fbf7ef28142cb5e1056d455149b
ericdwkim <ericdwkim.io@gmail.com>
2023-04-07 18:49:48 -0500

    Reach parqking website, type and submit license plate, type resident code.

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-print("hello world")
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+license_plate_to_register = os.getenv('TEST_LICENSE_PLATE')
+resident_parking_code = os.getenv('RESIDENT_PARKING_CODE')
+parqking_url = os.getenv('PARQKING_URL')
+
+
+options = webdriver.ChromeOptions()
+driver = uc.Chrome(use_subprocess=True, version_main=111, options=options)
+driver.get(parqking_url)
+
+license_plate_textbox_wait = WebDriverWait(driver, 300).until(
+    EC.visibility_of_element_located((By.CLASS_NAME, 'form-control uppercase alphanum')))
+
+license_plate_textbox_selector = driver.find_element(By.CLASS_NAME, 'form-control uppercase alphanum')
+license_plate_textbox_selector.click()
+license_plate_textbox_selector.send_keys(license_plate_to_register)
+
+submit_plate_btn = driver.find_element(By.CLASS_NAME, 'guest-pass btn btn-info btn-lg btn-block btn-rounded text-uppercase waves-effect waves-light guest-pass-enter')
+submit_plate_btn.click()
+
+enter_guest_code_modal_wait = WebDriverWait(driver, 150).until(
+    EC.visibility_of_element_located((By.CLASS_NAME, 'form-control uppercase')))
+enter_guest_code_modal = driver.find_element(By.CLASS_NAME, 'form-control uppercase')
+enter_guest_code_modal.click()
+enter_guest_code_modal.send_keys(resident_parking_code)
